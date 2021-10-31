82ce5dece067d637e9d09675d82180a3b01111a5
Chris Philbin <chris@chrisphilbin.net>
2021-10-31 08:32:04 -0400

    Adding vote confirmation upon submitting vote

--- src/components/movies/ShowMovieList.vue
+++ src/components/movies/ShowMovieList.vue
@@ -159,6 +159,25 @@
                 <p>Rover Dies</p>
               </button>
             </span>
+            <span v-else>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-12 w-12 text-blue-500 mx-auto mt-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <p class="text-blue-500 font-bold font-sans">
+                Vote received - Thanks!
+              </p>
+            </span>
           </div>
         </div>
       </div>
@@ -179,7 +198,7 @@
             class="block h-4 w-4 cursor-text"
             :class="[
               Math.round(movie.vote_average) / 2 >= i
-                ? 'text-blue-400'
+                ? 'text-green-500'
                 : 'text-gray-400',
             ]"
             xmlns="http://www.w3.org/2000/svg"
