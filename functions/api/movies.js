668a4b08ed425127108bf949f5e6fdd448695f09
Chris Philbin <chris@chrisphilbin.net>
2021-11-05 08:14:18 -0400

    Adding ability to track and increment search terms

--- functions/api/movies.js
+++ functions/api/movies.js
@@ -4,10 +4,10 @@ const { db } = require("../util/admin");
 
 exports.findOrCreateMovie = async (request, response) => {
   const moviesRef = await db.collection("movies");
-  const snapshot = await moviesRef
+  const moviesSnapshot = await moviesRef
     .where("movieId", "==", request.params.movieId)
     .get();
-  if (snapshot.empty) {
+  if (moviesSnapshot.empty) {
     const newMovie = {
       movieId: request.params.movieId,
       dogLives: 0,
@@ -22,7 +22,7 @@ exports.findOrCreateMovie = async (request, response) => {
         return response.status(200).json(responseMovie);
       });
   }
-  snapshot.forEach((doc) => {
+  moviesSnapshot.forEach((doc) => {
     let movieData = doc.data();
     return response.status(200).json(movieData);
   });
@@ -54,9 +54,28 @@ exports.incrementVoteCount = async (request, response) => {
 };
 
 exports.searchMovies = async (request, response) => {
+  const movieTitle = request.body.movieTitle.toLowerCase();
   if (request.body.movieTitle === "") {
     return response.status(400).json({ error: "Movie title cannot be blank." });
   }
+  console.log(request.body.movieTitle, "movie Title");
+  db.collection("searches")
+    .where("movieTitle", "==", movieTitle)
+    .limit(1)
+    .get()
+    .then((query) => {
+      if (query.empty) {
+        const newSearchTerm = {
+          movieTitle: movieTitle,
+          timesSearched: 1,
+        };
+        db.collection("searches").add(newSearchTerm);
+      }
+      const searchTerm = query.docs[0];
+      let updatedSearchTerm = searchTerm.data();
+      updatedSearchTerm.timesSearched += 1;
+      searchTerm.ref.update(updatedSearchTerm);
+    });
   try {
     let movieResponse = await fetch(
       `https://api.themoviedb.org/3/search/movie?api_key=${process.env.VUE_APP_MOVIE_API}&language=en-US&query=${request.body.movieTitle}&page=1&include_adult=false`
